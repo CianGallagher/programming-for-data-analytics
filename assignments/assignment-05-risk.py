462067d1783b279a989a7ea943d72671518ca3b1
Cian Gallagher <ciangall1@gmail.com>
2024-10-24 19:06:22 +0100

    implemented 6 sided dice_roll with seeded RNG

--- assignments/assignment-05-risk.py
+++ assignments/assignment-05-risk.py
@@ -1,15 +1,20 @@
 # Author cian Gallagher
 # A python programme that simulates the board game risk and plotting the results.
 
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-rounds = 1000
+battles = 1000
 attackers = 3
 defenders = 2
+dice = [1, 2, 3, 4, 5, 6]
 
-dice1 = [1, 2, 3, 4, 5, 6]
-dice2 = [1, 2, 3, 4, 5, 6]
-dice3 = [1, 2, 3, 4, 5, 6]
+seed = 1337
+rng = np.random.default_rng(seed)
 
+dice_roll = rng.choice(dice)
 
+attacker_rolls = ([])
+defender_rolls = ([])
+
+print(dice_roll)
