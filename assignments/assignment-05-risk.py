f7bdda4d9cace1547466511659611e7c07978807
Cian Gallagher <ciangall1@gmail.com>
2024-10-24 17:17:07 +0100

    Initial commit, mostly guess work on the outline of the game

---
+++ assignments/assignment-05-risk.py
@@ -0,0 +1,13 @@
+# Author cian Gallagher
+# A python programme that simulates the board game risk and plotting the results.
+
+import pandas as pd
+import numpy as np
+
+rounds = 1000
+attackers = 3
+defenders = 2
+
+dice1 = [1, 2, 3, 4, 5, 6]
+dice2 = [1, 2, 3, 4, 5, 6]
+
