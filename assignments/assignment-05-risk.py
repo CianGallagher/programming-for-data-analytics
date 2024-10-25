60d7fa561b7c57fc08d3982ef7932b07462fde7a
Cian Gallagher <ciangall1@gmail.com>
2024-10-25 16:02:56 +0100

    dice_roll random number generator moved inside the for loop and now basic 1 vs 1 rolling seems to be working

--- assignments/assignment-05-risk.py
+++ assignments/assignment-05-risk.py
@@ -12,14 +12,14 @@ dice = [1, 2, 3, 4, 5, 6]
 seed = 1337
 rng = np.random.default_rng(seed)
 
-dice_roll = rng.choice(dice)
-
 attacker_rolls = []
 defender_rolls = []
 
+
 for battle in range(battles):
-    print(dice_roll)
+    dice_roll = rng.choice(dice)
     attacker_rolls.append(dice_roll)
+    dice_roll = rng.choice(dice)
     defender_rolls.append(dice_roll)
 
 print(attacker_rolls)
