446308ffdae8bff446a4c74e08d834a6c4e004aa
CianGallagher <ciangall1@gmail.com>
2024-10-26 14:45:34 +0100

    moved the second loop into the first one and it worked! Need to do some testing on results and cleanup before plotting

--- assignments/assignment-05-risk.py
+++ assignments/assignment-05-risk.py
@@ -19,19 +19,23 @@ for battle in range(battles):
     attacker_roll = sorted(rng.choice(dice, size=attackers), reverse=True)
     defender_roll = sorted(rng.choice(dice, size=defenders), reverse=True)
 
-    faceoff.append((attacker_roll, defender_roll))
+    testzip = list(zip(attacker_roll, defender_roll))
+
+    for i in testzip:
+        print(i)  # Print each pair of rolls
+        if i[0] > i[1]:
+            print("Attacker Wins!")
+        else:
+            print("Defender Wins")
 
 # Probably need another loop - line up the two lists in each index of faceoff and write if statements for Risks' rules of engagement. 
   
-testzip = zip(attacker_roll, defender_roll)
-
-for i in testzip:
-    if i[0] > i[1]:
-        print("Attacker Wins!")
-    else:
-        print("Defender Wins")
+testzip = list(zip(attacker_roll, defender_roll))
 
 
+
+
+# print(len(testzip))
 # print(attacker_roll)
 # print(defender_roll)
 # print(faceoff)
