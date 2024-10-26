10c8e9dcd459d86bc85c791fd7c4e8dd1ab3d90f
CianGallagher <ciangall1@gmail.com>
2024-10-26 15:27:01 +0100

    enumerate saves the day, for loop looks to be fully working now, plotting looks to be next on the agenda

--- assignments/assignment-05-risk.py
+++ assignments/assignment-05-risk.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-battles = 5
+battles = 3
 attackers = 3
 defenders = 2
 dice = [1, 2, 3, 4, 5, 6]
@@ -12,29 +12,30 @@ dice = [1, 2, 3, 4, 5, 6]
 seed = 1337
 rng = np.random.default_rng(seed)
 
-faceoff = []
-
 for battle in range(battles):
 
-    attacker_roll = sorted(rng.choice(dice, size=attackers), reverse=True)
-    defender_roll = sorted(rng.choice(dice, size=defenders), reverse=True)
+    attacker_rolls = sorted(rng.choice(dice, size=attackers), reverse=True)
+    defender_rolls = sorted(rng.choice(dice, size=defenders), reverse=True)
 
-    testzip = list(zip(attacker_roll, defender_roll))
+
+
+    faceoff = list(zip(attacker_rolls, defender_rolls))
 
     print(f"Battle {battle + 1}:")
-    for i in testzip:
-        if i[0] > i[1]:
+    print(f"Attacker rolls: {attacker_rolls}")
+    print(f"Defender rolls: {defender_rolls}")
+    print("Dice pairs:", faceoff)
+
+    for i, (attacker_single_roll, defender_single_roll) in enumerate(faceoff):
+        if attacker_single_roll > defender_single_roll:
             print("Attacker Wins!")
         else:
             print("Defender Wins")
 
+
 # Probably need another loop - line up the two lists in each index of faceoff and write if statements for Risks' rules of engagement. 
   
-# testzip = list(zip(attacker_roll, defender_roll))
-
-# print(len(testzip))
 # print(attacker_roll)
 # print(defender_roll)
-# print(faceoff)
-# print(type(faceoff))
+
 
