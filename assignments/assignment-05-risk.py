13db527898b9a882b4cafa26f17657fc96ae37dd
CianGallagher <ciangall1@gmail.com>
2024-10-26 15:33:02 +0100

    modified print(dice_pairs) for readability in terminal

--- assignments/assignment-05-risk.py
+++ assignments/assignment-05-risk.py
@@ -17,21 +17,18 @@ for battle in range(battles):
     attacker_rolls = sorted(rng.choice(dice, size=attackers), reverse=True)
     defender_rolls = sorted(rng.choice(dice, size=defenders), reverse=True)
 
-
-
     faceoff = list(zip(attacker_rolls, defender_rolls))
 
     print(f"Battle {battle + 1}:")
     print(f"Attacker rolls: {attacker_rolls}")
     print(f"Defender rolls: {defender_rolls}")
-    print("Dice pairs:", faceoff)
+    print("Dice pairs:    ", faceoff)
 
     for i, (attacker_single_roll, defender_single_roll) in enumerate(faceoff):
         if attacker_single_roll > defender_single_roll:
             print("Attacker Wins!")
         else:
             print("Defender Wins")
-
 
 # Probably need another loop - line up the two lists in each index of faceoff and write if statements for Risks' rules of engagement. 
   
